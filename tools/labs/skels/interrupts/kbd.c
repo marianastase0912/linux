c88d8b7682b3c328fb159208b242275d109a15b1
marianastase0912 <maria_nastase1999@yahoo.com>
2022-03-24 09:32:09 +0200

    Lab4: ex2: declarat interrupt handler
    Signed-off-by: marianastase0912 <maria_nastase1999@yahoo.com>

--- tools/labs/skels/interrupts/kbd.c
+++ tools/labs/skels/interrupts/kbd.c
@@ -101,11 +101,15 @@ static inline u8 i8042_read_data(void)
 }
 
 /* TODO 2: implement interrupt handler */
+irqreturn_t kbd_interrupt_handler(int cnt, void *dev)
+{
+	pr_info("AJung aici\n");
 	/* TODO 3: read the scancode */
 	/* TODO 3: interpret the scancode */
 	/* TODO 3: display information about the keystrokes */
 	/* TODO 3: store ASCII key to buffer */
-
+	return IRQ_NONE;
+}
 static int kbd_open(struct inode *inode, struct file *file)
 {
 	struct kbd *data = container_of(inode->i_cdev, struct kbd, cdev);
@@ -167,7 +171,7 @@ static int kbd_init(void)
 	/* TODO 3: initialize spinlock */
 
 	/* TODO 2: Register IRQ handler for keyboard IRQ (IRQ 1). */
-
+	err = request_irq(I8042_KBD_IRQ, kbd_interrupt_handler, IRQF_SHARED, "ceva", &devs[0]);
 	cdev_init(&devs[0].cdev, &kbd_fops);
 	cdev_add(&devs[0].cdev, MKDEV(KBD_MAJOR, KBD_MINOR), 1);
 
@@ -175,6 +179,10 @@ static int kbd_init(void)
 	return 0;
 
 	/*TODO 2: release regions in case of error */
+out_release_regions:
+	release_region(I8042_STATUS_REG + 1, 1);
+	release_region(I8042_DATA_REG + 1, 1);
+
 
 out_unregister:
 	unregister_chrdev_region(MKDEV(KBD_MAJOR, KBD_MINOR),
@@ -189,6 +197,7 @@ static void kbd_exit(void)
 
 	/* TODO 2: Free IRQ. */
 
+	free_irq(I8042_KBD_IRQ, &devs[0]);
 	/* TODO 1: release keyboard I/O ports */
 
 	release_region(I8042_STATUS_REG + 1, 1);
