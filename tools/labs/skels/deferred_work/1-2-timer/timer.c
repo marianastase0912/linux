16c9b1acb54c7ce988fe04b8537ffab8db015cc2
marianastase0912 <maria_nastase1999@yahoo.com>
2022-04-01 09:02:44 +0300

    lab5: ex 1-2: TIMER AND PERIODIC TIMER
    Signed-off-by: marianastase0912 <maria_nastase1999@yahoo.com>

--- tools/labs/skels/deferred_work/1-2-timer/timer.c
+++ tools/labs/skels/deferred_work/1-2-timer/timer.c
@@ -8,6 +8,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/jiffies.h>
 
 MODULE_DESCRIPTION("Simple kernel timer");
 MODULE_AUTHOR("SO2");
@@ -20,8 +21,11 @@ static struct timer_list timer;
 static void timer_handler(struct timer_list *tl)
 {
 	/* TODO 1: print a message */
+	pr_info("I got here at this time\n");
 
 	/* TODO 2: rechedule timer */
+	mod_timer(&timer, jiffies + TIMER_TIMEOUT * HZ);
+
 }
 
 static int __init timer_init(void)
@@ -29,9 +33,11 @@ static int __init timer_init(void)
 	pr_info("[timer_init] Init module\n");
 
 	/* TODO 1: initialize timer */
+	unsigned int flags = 0;
+	timer_setup(&timer, timer_handler, flags);
 
 	/* TODO 1: schedule timer for the first time */
-
+	mod_timer(&timer, jiffies + TIMER_TIMEOUT * HZ);
 	return 0;
 }
 
@@ -40,6 +46,7 @@ static void __exit timer_exit(void)
 	pr_info("[timer_exit] Exit module\n");
 
 	/* TODO 1: cleanup; make sure the timer is not running after we exit */
+	del_timer_sync(&timer);
 }
 
 module_init(timer_init);
