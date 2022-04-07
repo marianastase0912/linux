3d439f61e5f94449663411607c0ce5e153b29b73
marianastase0912 <maria_nastase1999@yahoo.com>
2022-04-07 18:46:12 +0300

    Lab5: ex5: Workqueues, created work_handler and used it instead of simply alloc_io() directly to avoid the error
    Signed-off-by: marianastase0912 <maria_nastase1999@yahoo.com>

--- tools/labs/skels/deferred_work/3-4-5-deferred/kernel/deferred.c
+++ tools/labs/skels/deferred_work/3-4-5-deferred/kernel/deferred.c
@@ -42,6 +42,7 @@ static struct my_device_data {
 	/* TODO 2: add flag */
 	int flag;
 	/* TODO 3: add work */
+	struct work_struct work;
 	/* TODO 4: add list for monitored processes */
 	/* TODO 4: add spinlock to protect list */
 } dev;
@@ -76,7 +77,10 @@ static struct mon_proc *get_proc(pid_t pid)
 
 
 /* TODO 3: define work handler */
-
+static void handler_work(struct work_struct *w)
+{
+	alloc_io();
+}
 #define ALLOC_IO_DIRECT
 /* TODO 3: undef ALLOC_IO_DIRECT*/
 
@@ -93,10 +97,12 @@ static void timer_handler(struct timer_list *tl)
 			break;
 		case TIMER_TYPE_ALLOC:
 			pr_info("Are you here\n");
-			alloc_io();
-			break;
+			// alloc_io();
 	
 		/* TODO 3: schedule work */
+			schedule_work(&data->work);
+
+			break;
 		/* TODO 4: iterate the list and check the proccess state */
 			/* TODO 4: if task is dead print info ... */
 			/* TODO 4: ... decrement task usage counter ... */
@@ -179,7 +185,7 @@ static int deferred_init(void)
 	/* TODO 2: Initialize flag. */
 	dev.flag = TIMER_TYPE_NONE;
 	/* TODO 3: Initialize work. */
-
+	INIT_WORK(&dev.work, handler_work);
 	/* TODO 4: Initialize lock and list. */
 
 	cdev_init(&dev.cdev, &my_fops);
@@ -202,7 +208,7 @@ static void deferred_exit(void)
 	/* TODO 1: Cleanup: make sure the timer is not running after exiting. */
 	del_timer_sync(&dev.timer);
 	/* TODO 3: Cleanup: make sure the work handler is not scheduled. */
-
+	flush_scheduled_work();
 	/* TODO 4: Cleanup the monitered process list */
 		/* TODO 4: ... decrement task usage counter ... */
 		/* TODO 4: ... remove it from the list ... */
