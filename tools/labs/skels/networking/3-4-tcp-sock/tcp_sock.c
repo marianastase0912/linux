395e4663515f0e48760c79770c1afb0d35d4237c
marianastase0912 <maria_nastase1999@yahoo.com>
2022-05-16 00:18:07 +0300

    LAB10: ex 4: Accepting connections in kernel space
    Signed-off-by: marianastase0912 <maria_nastase1999@yahoo.com>

--- tools/labs/skels/networking/3-4-tcp-sock/tcp_sock.c
+++ tools/labs/skels/networking/3-4-tcp-sock/tcp_sock.c
@@ -80,15 +80,29 @@ int __init my_tcp_sock_init(void)
 		goto out;
 	}
 	/* TODO 2: create new socket for the accepted connection */
+	err = sock_create_lite(PF_INET, SOCK_STREAM, IPPROTO_TCP, &new_sock);
+	if (err < 0) {
+		goto out;
+	}
+	new_sock->ops = sock->ops;
 
 	/* TODO 2: accept a connection */
-
+	err = sock->ops->accept(sock, new_sock, 0, true);
+	if (err < 0) {
+		goto out;
+	}
 	/* TODO 2: get the address of the peer and print it */
+	err = sock->ops->getname(new_sock, (struct sockaddr *)&raddr, 1);
+	if (err < 0) {
+		goto out_release_new_sock;
+	}
 
+	print_sock_address(raddr);
 	return 0;
 
 out_release_new_sock:
 	/* TODO 2: cleanup socket for accepted connection */
+	sock_release(new_sock);
 out_release:
 	/* TODO 1: cleanup listening socket */
 	sock_release(sock);
@@ -99,7 +113,7 @@ out:
 void __exit my_tcp_sock_exit(void)
 {
 	/* TODO 2: cleanup socket for accepted connection */
-
+	sock_release(new_sock);
 	/* TODO 1: cleanup listening socket */
 	sock_release(sock);
 }
